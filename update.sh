8a12608c1993df3ab3300d463a9f593883f020b5
Scynk <scynk159@gmail.com>
2020-01-27 14:04:26 +0100

    Add update.sh

---
+++ update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sciezka=~/Configs
+log=aktualizacje.txt
+
+# Aktualizacja repozytorium
+cd $sciezka
+date >> $sciezka/$log
+git pull >> $sciezka/$log
+echo "- - - - - - - - - -" >> $sciezka/$log
+echo "" >> $sciezka/$log
+
+# Aktualizacja pliku .bashrc
+cd ~/
+cat .bashrc-template > .bashrc
+echo "" >> .bashrc
+cat $sciezka/.bashrc >> .bashrc
+echo "" >> .bashrc
+
+# Dodanie ustawień tylko dla Raspberry PI
+if [ "`whoami`" = "pi" ]; then
+    cat $sciezka/.bashrc-rpi >> .bashrc
+    echo "" >> .bashrc
+fi
+
+# Przeładowanie pliku .bashrc
+source ~/.bashrc
